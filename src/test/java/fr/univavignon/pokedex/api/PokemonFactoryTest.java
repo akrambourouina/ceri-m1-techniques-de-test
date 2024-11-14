73407d20338e2aa83cddfd135fc9e446ed52b7f6
akrambourouina <akram.bourouina@alumni.univ-avignon.fr>
2024-11-14 11:30:33 +0100

    mise a jour

--- src/test/java/fr/univavignon/pokedex/api/PokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokemonFactoryTest.java
@@ -18,12 +18,12 @@ public class PokemonFactoryTest {
         Pokemon createdPokemon = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
 
         assertNotNull(createdPokemon);
-        assertEquals(0, createdPokemon.getIndex());
-        assertEquals("NomPokemon", createdPokemon.getName());
-        assertEquals(4, createdPokemon.getCp());
-        assertEquals(64, createdPokemon.getHp());
-        assertEquals(4000, createdPokemon.getDust());
-        assertEquals(4, createdPokemon.getCandy());
+        assertEquals(1, createdPokemon.getIndex());
+        assertEquals("Salamèche", createdPokemon.getName());
+        assertEquals(500, createdPokemon.getCp());
+        assertEquals(50, createdPokemon.getHp());
+        assertEquals(2000, createdPokemon.getDust());
+        assertEquals(3, createdPokemon.getCandy());
 
         // Les IVs sont aléatoires, donc nous vérifions qu'ils sont dans la plage attendue
         assertTrue(createdPokemon.getAttack() >= 0 && createdPokemon.getAttack() <= 15);
