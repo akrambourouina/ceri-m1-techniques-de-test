c3412fd61c373f865286bc082a0af900cd68fbeb
akrambourouina <akram.bourouina@alumni.univ-avignon.fr>
2024-11-14 12:48:44 +0100

    mise a jour

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -2,35 +2,41 @@ package fr.univavignon.pokedex.api;
 
 /**
  * Implementation of the IPokemonFactory interface.
+ *
+ * @author fv
  */
-import java.util.Random;
-
 public class PokemonFactory implements IPokemonFactory {
-
-    private final Random random;
-
-    // Constructeur pour injection du générateur de nombres aléatoires
-    public PokemonFactory(Random random) {
-        this.random = random;
-    }
-
-    // Si aucun générateur n'est passé, utilise un générateur par défaut
-    public PokemonFactory() {
-        this.random = new Random();
-    }
 
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-        // Logique de calcul pour les IVs
-        int attack = random.nextInt(16);  // Genère un nombre entre 0 et 15
-        int defense = random.nextInt(16);  // Genère un nombre entre 0 et 15
-        int stamina = random.nextInt(16);  // Genère un nombre entre 0 et 15
+        try {
 
-        // Calcul du pourcentage des IVs
-        double ivPercentage = (attack + defense + stamina) / 45.0 * 100;
+            PokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
+            // Retrieve metadata for the given Pokemon index
+            PokemonMetadata metadata = metadataProvider.getPokemonMetadata(index);
 
-        // Crée un Pokémon avec les attributs fournis, y compris le CP
-        return new Pokemon(index, "NomPokemon", cp, hp, dust, candy, attack, defense, stamina, ivPercentage);
+            // Calculate IV (this is just an example, the actual calculation might depend on other factors)
+            double iv = ((metadata.getAttack()+metadata.getDefense()+metadata.getStamina())/45.0);
+
+            // Create and return the Pokemon instance with the computed IV
+            return new Pokemon(
+                    index,
+                    metadata.getName(),
+                    metadata.getAttack(),
+                    metadata.getDefense(),
+                    metadata.getStamina(),
+                    cp,
+                    hp,
+                    dust,
+                    candy,
+                    iv
+            );
+        } catch (PokedexException e) {
+            // Handle error if the metadata for the given index is not found
+            System.err.println("Error creating Pokemon: " + e.getMessage());
+            return null;
+        }
     }
-}
 
+
+}
