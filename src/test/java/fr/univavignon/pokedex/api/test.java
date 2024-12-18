d0b24f6b3ab47d87f84dfaeca2646657e9237078
akrambourouina <akram.bourouina@alumni.univ-avignon.fr>
2024-12-19 00:00:19 +0100

    mise a jour TP5

--- src/test/java/fr/univavignon/pokedex/api/test.java
+++ src/test/java/fr/univavignon/pokedex/api/test.java
@@ -22,31 +22,47 @@ public class test {
 	}
 
 	@Test
-	public void testCreatePokemon() throws PokedexException {
-		// Définir un Pokémon fictif pour l'index 0 (par exemple, Bulbasaur)
-		PokemonMetadata mockMetadata = new PokemonMetadata(0, "Bulbasaur", 126, 126, 90);
-		when(mockMetadataProvider.getPokemonMetadata(0)).thenReturn(mockMetadata);
+	public void testCreatePokemonValid() throws PokedexException {
+		// Préparer des métadonnées fictives pour un Pokémon (par exemple, Bulbasaur)
+		PokemonMetadata mockMetadata = new PokemonMetadata(1, "Bulbasaur", 126, 126, 90);
+		when(mockMetadataProvider.getPokemonMetadata(1)).thenReturn(mockMetadata);
 
-		// Appeler la méthode createPokemon pour obtenir un Pokémon
-		Pokemon pokemon = pokemonFactory.createPokemon(0, 150, 100, 200, 25);
+		// Créer le Pokémon avec des paramètres valides
+		Pokemon pokemon = pokemonFactory.createPokemon(1, 150, 100, 200, 25);
 
-		// Vérifier que le Pokémon a été correctement créé
+		// Vérifier que le Pokémon a bien été créé
 		assertNotNull(pokemon);
-		assertEquals(0, pokemon.getIndex());
 		assertEquals("Bulbasaur", pokemon.getName());
-		assertEquals(126, pokemon.getAttack());
-		assertEquals(126, pokemon.getDefense());
-		assertEquals(90, pokemon.getStamina());
 		assertEquals(150, pokemon.getCp());
 		assertEquals(100, pokemon.getHp());
 		assertEquals(200, pokemon.getDust());
 		assertEquals(25, pokemon.getCandy());
+	}
+	@Test
+	public void testCreatePokemonIvCalculation() throws PokedexException {
+		// Préparer des métadonnées fictives pour un Pokémon (par exemple, Bulbasaur)
+		PokemonMetadata mockMetadata = new PokemonMetadata(1, "Bulbasaur", 126, 126, 90);
+		when(mockMetadataProvider.getPokemonMetadata(1)).thenReturn(mockMetadata);
 
-		// Vérifier que l'IV a été calculé correctement (à partir de l'exemple de formule)
-		double expectedIV = (126 + 126 + 90) / 45.0;
-		assertEquals(expectedIV, pokemon.getIv(), 0.01);
+		// Créer le Pokémon
+		Pokemon pokemon = pokemonFactory.createPokemon(1, 150, 100, 200, 25);
+
+		// Calculer l'IV attendu
+		double expectedIv = (126 + 126 + 90) / 45.0;
+
+		// Vérifier que l'IV est calculé correctement
+		assertEquals(expectedIv, pokemon.getIv(), 0.01);
 	}
 
+	@Test
+	public void testCreatePokemonWithMissingMetadata() throws PokedexException {
+		// Utiliser un index invalide pour simuler le Pokémon "MISSINGNO"
+		Pokemon pokemon = pokemonFactory.createPokemon(0, 150, 100, 200, 25);
+
+		// Vérifier que le nom du Pokémon est "MISSINGNO"
+		assertNotNull(pokemon);
+		assertEquals("MISSINGNO", pokemon.getName());
+	}
 	@Test
 	public void testCreatePokemonWithInvalidIndex() throws PokedexException {
 		// Configurer un index qui ne correspond à aucun Pokémon
